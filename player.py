11638285e4df2894f944d62290fcdfb6b8a326f4
Daniel Fonseca Alves <dalraf@gmail.com>
2023-04-26 17:05:42 -0300

    Adcionado freeze map

--- player.py
+++ player.py
@@ -19,6 +19,7 @@ class Player:
         self.shotting = 'shotting'
         self.status = self.stopped
         self.old_status = self.stopped
+        self.freeze_map = False
 
     def tile_coord(self, x, y):
         return x * self.tile_size, y * self.tile_size
@@ -55,19 +56,34 @@ class Player:
     def walk_stopped(self):
         self.status = self.stopped
 
+    def verify_freeze_map(self, active=False):
+        if active:
+            self.freeze_map = True
+        if self.freeze_map:
+            if (self.index_map // self.sprint) < (len(self.map) - 1):
+                return False
+            else:
+                self.freeze_map = False
+                return True
+        else:
+            return True
+
     def define_map(self):
-        if self.status != self.old_status:
-            self.index_map = 0
-            self.old_status = self.status
+        if self.verify_freeze_map():
+            if self.status != self.old_status:
+                self.index_map = 0
+                self.old_status = self.status
 
-        if self.status == self.stopped:
-            self.map = self.map_stopped
+            if self.status == self.stopped:
+                self.map = self.map_stopped
 
-        if self.status == self.walking:
-            self.map = self.map_walking
+            if self.status == self.walking:
+                self.map = self.map_walking
 
-        if self.status == self.shotting:
-            self.map = self.map_shotting
+            if self.status == self.shotting:
+                self.map = self.map_shotting
+                self.verify_freeze_map(active=True)
+
 
     def draw(self):
         self.define_map()
